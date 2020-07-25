175a3ecc4bb8e82ddfa913b9e68d6191d488d0ff
Zainul Reza <62101121+zainulreza@users.noreply.github.com>
2020-07-25 17:05:09 +0530

    new

--- resources/views/layouts/frontend/Partial/footer.blade.php
+++ resources/views/layouts/frontend/Partial/footer.blade.php
@@ -7,8 +7,8 @@
 					<div class="footer-section">
 
 						<a class="logo" href="#"><img src="images/logo.png" alt="Logo Image"></a>
-						<p class="copyright">Bona @ 2017. All rights reserved.</p>
-						<p class="copyright">Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></p>
+						<p class="copyright">ZainBlog @ 2020. All rights reserved.</p>
+						<p class="copyright">Designed by <a href="#" target="_blank">Zainul Reza</a></p>
 						<ul class="icons">
 							<li><a href="#"><i class="ion-social-facebook-outline"></i></a></li>
 							<li><a href="#"><i class="ion-social-twitter-outline"></i></a></li>
